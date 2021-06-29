9008beaca67247e5650fc7f1c27cf19ac4838414
Oleksandr Shyrokov <oleksandr.shyrokov@ctdev.io>
2021-06-29 15:09:36 +0300

    fix test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
+// @ts-ignore
 import renderer from 'react-test-renderer';
-import { Platform } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 import RNBlurredText from '../index';
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 20,
+        color: 'rgb(20,200,220)',
+    }
+});
 
 describe('<RNBlurredText />', () => {
   const testText = 'Blurred text';
-  const textSize = 20;
   const blurrVal = 4;
-  const textColor= 'rgb(20,200,220)';
 
   it('Should display text for android', () => {
     Platform.select=jest.fn().mockImplementation((obj) => obj.android);
 
     const testRenderer = renderer.create(
       <RNBlurredText
-      textVal={testText}
-      textSize={textSize}
-      blurrVal={blurrVal}
-      textColor={textColor}
+      style={styles.textStyle}
+      value={testText}
+      blurr={blurrVal}
       />
     );
     expect (testRenderer.toJSON()).toMatchSnapshot();
@@ -28,13 +33,11 @@ describe('<RNBlurredText />', () => {
 
     const testRenderer = renderer.create(
       <RNBlurredText
-      textVal={testText}
-      textSize={textSize}
-      blurrVal={blurrVal}
-      textColor={textColor}
+      style={styles.textStyle}
+      value={testText}
+      blurr={blurrVal}
       />
     );
     expect (testRenderer.toJSON()).toMatchSnapshot();
   });
 });
-
