2d3ab5cd1611f727fe27883a6daa6be5998d7c0a
Oleksandr Shyrokov <oleksandr.shyrokov@ctdev.io>
2021-06-29 12:44:56 +0300

    ios bug with resizing fixed

--- ios/BlurredTextViewManager.m
+++ ios/BlurredTextViewManager.m
@@ -8,38 +8,44 @@
 
 
 RCT_EXPORT_MODULE(BlurredTextView)
+BlurredUILabel *label;
+UIImageView *imageView;
 
 - (UILabel *)view
 {
-    BlurredUILabel *label = [[BlurredUILabel alloc] init];
-    
+    label = [[BlurredUILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    [label blurryText];
     return label;
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(value, NSString, UILabel)
 {
   view.text = json;
+  [label blurryText];
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(blurr, NSString, BlurredUILabel)
 {
   view.blurRadius = [json floatValue];
+  [label blurryText];
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(fontSize, NSString, UILabel)
 {
     view.font = [UIFont systemFontOfSize:[json floatValue]];
-    [UIFont fontWithName:view.font.fontName size:[json floatValue]];
+    [label blurryText];
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(color, UIColor, UILabel)
 {
     view.textColor = json ? [RCTConvert UIColor:json] : defaultView.textColor;
+    [label blurryText];
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(fontFamily, NSString, UILabel)
 {
     view.font = [UIFont fontWithName:json size:view.font.pointSize];
+    [label blurryText];
 }
 
 @end
--- ios/BlurredUILabel.m
+++ ios/BlurredUILabel.m
@@ -1,30 +1,42 @@
 #import "BlurredUILabel.h"
 
 @implementation BlurredUILabel
-
-- (void)setText:(NSString *)text {
-    super.text = text;
+- (void)blurryText {
     UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, [[UIScreen mainScreen] scale]);
-    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
     
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    [self.layer renderInContext: UIGraphicsGetCurrentContext()];
+    UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    
-    CIFilter *blurFilter = [CIFilter filterWithName:@"CIGaussianBlur"];
-    [blurFilter setDefaults];
-    
-    CIImage *imageToBlur = [CIImage imageWithCGImage: image.CGImage];
-    
-    [blurFilter setValue:imageToBlur forKey:kCIInputImageKey];
-    [blurFilter setValue:@(self.blurRadius) forKey:@"inputRadius"];
-
-    CIImage *outputImage = blurFilter.outputImage;
-    CIContext *context = [CIContext contextWithOptions:nil];
-    CGRect cropRect2 = CGRectMake(0, 0, self.bounds.size.width*3, self.bounds.size.height * 2);
-    CGImageRef cgimg = [context createCGImage:outputImage fromRect:cropRect2];
-
+    CIFilter * gaussianBlurFilter = [CIFilter filterWithName:@"CIGaussianBlur"];
+    [gaussianBlurFilter setDefaults];
+    CIImage * inputImage = [CIImage imageWithCGImage:[image CGImage]];
+    [gaussianBlurFilter setValue:inputImage forKey:kCIInputImageKey];
+    [gaussianBlurFilter setValue:@(self.blurRadius) forKey:kCIInputRadiusKey];
+    CIImage * outputImage = [gaussianBlurFilter outputImage];
+    CIContext * context = [CIContext contextWithOptions:nil];
+    CGImageRef cgimg = [context createCGImage:outputImage fromRect:[inputImage extent]];
     [self.layer setContents:(__bridge id)cgimg];
     CGImageRelease(cgimg);
+}
+
+-(UIImage *)getBlurryImageText {
+    NSLog(@"%@", @"GET_BLURRY_IMAGE_TEXTTTT");
+    UIGraphicsBeginImageContext(self.bounds.size);
+    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    CIFilter * gaussianBlurFilter = [CIFilter filterWithName:@"CIGaussianBlur"];
+    [gaussianBlurFilter setDefaults];
+    CIImage * inputImage = [CIImage imageWithCGImage:[image CGImage]];
+    [gaussianBlurFilter setValue:inputImage forKey:kCIInputImageKey];
+    [gaussianBlurFilter setValue:@(self.blurRadius) forKey:kCIInputRadiusKey];
+    CIImage * outputImage = [gaussianBlurFilter outputImage];
+    CIContext * context = [CIContext contextWithOptions:nil];
+    CGImageRef cgimg = [context createCGImage:outputImage fromRect:[inputImage extent]];
+    UIImage * convertedImage = [UIImage imageWithCGImage:cgimg];
+    CGImageRelease(cgimg);
+    return convertedImage;
 }
 
 @end
