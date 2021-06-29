bb477fffb1a845b2add323899556a0658004edc8
Oleksandr Shyrokov <oleksandr.shyrokov@ctdev.io>
2021-06-29 15:32:12 +0300

    fix H file

--- ios/BlurredUILabel.h
+++ ios/BlurredUILabel.h
@@ -1,7 +1,9 @@
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 @interface BlurredUILabel : UILabel
 
 @property (nonatomic, readwrite) IBInspectable CGFloat blurRadius;
-
+-(UIImage *)getBlurryImageText;
+- (void)blurryText;
 @end
